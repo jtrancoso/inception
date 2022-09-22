7903685d6a68ce940b2e686ccfce8f8deddc5366
jtrancos <jesus.trancoso.jimenez@gmail.com>
2022-09-22 17:21:32 +0200

    mariadb script works, lacks wp content

--- srcs/requirements/mariadb/config/start.sh
+++ srcs/requirements/mariadb/config/start.sh
@@ -12,11 +12,11 @@ Y
 Y
 EOF
 
-mysql -e "CREATE $DB_NAME charset utf8mb4 collate utf8mb4_unicode_ci"
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME charset utf8mb4 collate utf8mb4_unicode_ci"
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASS'"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'"
-mysql -e "CREATE USER '$WP_USER'@'%' IDENTIFIED BY '$WP_USER_PASS'"
-mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$WP_USER'@'%'"
+mysql -e "CREATE USER IF NOT EXISTS '$WP_USER'@'%' IDENTIFIED BY '$WP_USER_PASS'"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$WP_USER'@'%'"
 mysql -e "FLUSH PRIVILEGES"
 service mysql stop
 mysqld_safe
