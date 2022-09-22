764fb3401ff5f5de8776ec7e5433f66a9e885325
jtrancos <jesus.trancoso.jimenez@gmail.com>
2022-09-22 14:34:15 +0200

    testing db

--- srcs/requirements/wordpress/config/start_wp.sh
+++ srcs/requirements/wordpress/config/start_wp.sh
@@ -2,9 +2,9 @@
 
 tar xf /latest.tar.gz -C /var/www/html
 mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-sed -i "s/database_name_here/wordpress/g" /var/www/html/wordpress/wp-config.php
-sed -i "s/username_here/user/g" /var/www/html/wordpress/wp-config.php
-sed -i "s/password_here/1234/g" /var/www/html/wordpress/wp-config.php
-sed -i "s/localhost/mariadb/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/username_here/$DB_USER/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/password_here/$DB_USER_NAME/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/localhost/$DB_HOST/g" /var/www/html/wordpress/wp-config.php
 chown www-data:www-data /var/www/html/wordpress/wp-config.php -R
 php-fpm7.3 -F
