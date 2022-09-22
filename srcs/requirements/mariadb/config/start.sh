68f6942929a4ba22e5342e28222da5b968648932
jtrancos <jesus.trancoso.jimenez@gmail.com>
2022-09-22 13:32:29 +0200

    db created?

--- srcs/requirements/mariadb/config/start.sh
+++ srcs/requirements/mariadb/config/start.sh
@@ -12,16 +12,11 @@ Y
 Y
 EOF
 
-result=$(sudo mysql -s -N -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$DB_NAME'");
-if [ -z "$result" ]; then
-  sudo mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME charset utf8mb4 collate utf8mb4_unicode_ci"
-  sudo mysql $DB_NAME < /wordpress.sql
-fi
+sudo mysql -e "CREATE $DB_NAME charset utf8mb4 collate utf8mb4_unicode_ci"
 sudo mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASS'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON * . * TO '$DB_USER'@'%'"
 sudo mysql -e "CREATE USER '$WP_USER'@'%' IDENTIFIED BY '$WP_USER_PASS'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$WP_USER'@'%'"
 sudo mysql -e "FLUSH PRIVILEGES"
-echo **Database Created**
 sudo service mysql stop
 sudo mysqld_safe
