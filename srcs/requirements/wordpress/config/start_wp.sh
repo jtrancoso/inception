2caaa7867e41e0878a391090d7bfd7fcc878c8a3
jtrancos <jesus.trancoso.jimenez@gmail.com>
2022-10-03 13:21:31 +0200

    removed cli

--- srcs/requirements/wordpress/config/start_wp.sh
+++ srcs/requirements/wordpress/config/start_wp.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
 tar xf /latest.tar.gz -C /var/www/html
-mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wordpress/wp-config.php
-sed -i "s/username_here/$DB_USER/g" /var/www/html/wordpress/wp-config.php
-sed -i "s/password_here/$DB_USER_NAME/g" /var/www/html/wordpress/wp-config.php
-sed -i "s/localhost/$DB_HOST/g" /var/www/html/wordpress/wp-config.php
+# mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+# sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wordpress/wp-config.php
+# sed -i "s/username_here/$DB_USER/g" /var/www/html/wordpress/wp-config.php
+# sed -i "s/password_here/$DB_USER_NAME/g" /var/www/html/wordpress/wp-config.php
+# sed -i "s/localhost/$DB_HOST/g" /var/www/html/wordpress/wp-config.php
 chown www-data:www-data /var/www/html/wordpress/wp-config.php -R
 php-fpm7.3 -F
 wp db create
