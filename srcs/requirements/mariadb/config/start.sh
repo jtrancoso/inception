ca0cb1efbf4fe26ab4cf0e07f4224de0154ecb2b
jtrancos <jesus.trancoso.jimenez@gmail.com>
2022-10-03 16:16:13 +0200

    db is real

--- srcs/requirements/mariadb/config/start.sh
+++ srcs/requirements/mariadb/config/start.sh
@@ -11,7 +11,11 @@ Y
 Y
 Y
 EOF
-
+result=$(sudo mysql -s -N -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$DB_NAME'");
+if [ -z "$result" ]; then
+  sudo mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME charset utf8mb4 collate utf8mb4_unicode_ci"
+  sudo mysql $DB_NAME < /wordpress.sql
+fi
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME charset utf8mb4 collate utf8mb4_unicode_ci"
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASS'"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'"
