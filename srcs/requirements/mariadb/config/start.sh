4979d19baca6d041ddc5a85ce362ec47e01b41b0
jtrancos <jesus.trancoso.jimenez@gmail.com>
2022-10-05 16:39:03 +0200

    mysql secured root

--- srcs/requirements/mariadb/config/start.sh
+++ srcs/requirements/mariadb/config/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-service mysql start
-mysql_secure_installation << EOF
+sudo service mysql start
+sudo mysql_secure_installation << EOF
 
 Y
 $DB_ROOT_PASS
@@ -16,10 +16,10 @@ if [ -z "$result" ]; then
   sudo mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME charset utf8mb4 collate utf8mb4_unicode_ci"
   sudo mysql $DB_NAME < /wordpress.sql
 fi
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASS'"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'"
-mysql -e "CREATE USER IF NOT EXISTS '$WP_USER'@'%' IDENTIFIED BY '$WP_USER_PASS'"
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$WP_USER'@'%'"
-mysql -e "FLUSH PRIVILEGES"
-service mysql stop
-mysqld_safe
+sudo mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASS'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'"
+sudo mysql -e "CREATE USER IF NOT EXISTS '$WP_USER'@'%' IDENTIFIED BY '$WP_USER_PASS'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$WP_USER'@'%'"
+sudo mysql -e "FLUSH PRIVILEGES"
+sudo service mysql stop
+sudo mysqld_safe
