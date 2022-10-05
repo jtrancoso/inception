fc663a36fc6c23ca1518a227179cff81b01dbaad
jtrancos <jesus.trancoso.jimenez@gmail.com>
2022-10-05 12:03:41 +0200

    solved db user

--- srcs/requirements/mariadb/config/start.sh
+++ srcs/requirements/mariadb/config/start.sh
@@ -16,7 +16,6 @@ if [ -z "$result" ]; then
   sudo mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME charset utf8mb4 collate utf8mb4_unicode_ci"
   sudo mysql $DB_NAME < /wordpress.sql
 fi
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME charset utf8mb4 collate utf8mb4_unicode_ci"
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASS'"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'"
 mysql -e "CREATE USER IF NOT EXISTS '$WP_USER'@'%' IDENTIFIED BY '$WP_USER_PASS'"
